45d7c08e58ad125c97a7e5ef9fcaebae468e5296
Manisha2505 <manishachandra.25@gmail.com>
2025-03-06 17:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
 //complete this code
-class Person {}
+// class Person {}
 
-class Student extends Person {}
+// class Student extends Person {}
 
-class Teacher extends Person {}
+// class Teacher extends Person {}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
+
+
+// Base class Person
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+// Student class inheriting from Person
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher class inheriting from Person
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Example Usage
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
